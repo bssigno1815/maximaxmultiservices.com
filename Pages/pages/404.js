ec26e9d43ccb4688f76f078ff919cc3b9cf98ed3
bssigno1815 <briyantsigno@gmail.com>
2025-08-22 04:52:28 -0400

    Pages/404.js

---
+++ Pages/pages/404.js
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>404 - Paj pa jwenn</h1>
+      <p>Eskize, paj ou chache a pa egziste.</p>
+    </div>
+  );
+}
